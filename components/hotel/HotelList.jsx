b5a066ac4daafc0ae50553253f615835f00bfede
Asif-102 <asif1zero2@gmail.com>
2024-12-26 15:27:10 +0600

    connected db and test it

--- components/hotel/HotelList.jsx
+++ components/hotel/HotelList.jsx
@@ -1,10 +1,14 @@
+import { getAllHotels } from "@/database/queries";
 import HotelCard from "./HotelCard";
 
-const HotelList = () => {
+const HotelList = async () => {
+  const allHotels = await getAllHotels();
+  console.log("🚀 ~ HotelList ~ allHotels:", allHotels);
+
   return (
     <div className="col-span-9">
       <div className="space-y-4">
-       <HotelCard />
+        <HotelCard />
       </div>
     </div>
   );
