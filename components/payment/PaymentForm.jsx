c82a5f6bf58bbb74b4fd3a1223d111aa650d72e4
Asif-102 <asif1zero2@gmail.com>
2024-12-28 00:36:04 +0600

    booking feature complete

--- components/payment/PaymentForm.jsx
+++ components/payment/PaymentForm.jsx
@@ -1,6 +1,48 @@
-const PaymentForm = ({ checkin, checkout }) => {
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+const PaymentForm = ({ loggedInUser, hotelInfo, checkin, checkout }) => {
+  const router = useRouter();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+
+    const hotelId = hotelInfo?.id;
+    const userId = loggedInUser?.id;
+    const checkin = formData.get("checkin");
+    const checkout = formData.get("checkout");
+
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/payment", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          hotelId,
+          userId,
+          checkin,
+          checkout,
+        }),
+      });
+
+      res.status === 201 && router.push("/bookings");
+    } catch (error) {
+      console.error(error);
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <form className="my-8">
+    <form className="my-8" onSubmit={onSubmit}>
       <div className="my-4 space-y-2">
         <label htmlFor="name" className="block">
           Name
@@ -8,6 +50,7 @@ const PaymentForm = ({ checkin, checkout }) => {
         <input
           type="text"
           id="name"
+          value={loggedInUser?.name}
           className="w-full border border-[#CCCCCC]/60 py-1 px-2 rounded-md"
         />
       </div>
@@ -19,6 +62,7 @@ const PaymentForm = ({ checkin, checkout }) => {
         <input
           type="email"
           id="email"
+          value={loggedInUser?.email}
           className="w-full border border-[#CCCCCC]/60 py-1 px-2 rounded-md"
         />
       </div>
@@ -70,8 +114,8 @@ const PaymentForm = ({ checkin, checkout }) => {
         />
       </div>
 
-      <button type="submit" className="btn-primary w-full">
-        Pay Now ($10)
+      <button disabled={loading} type="submit" className="btn-primary w-full">
+        {loading ? "wait..." : "Pay Now ($10)"}
       </button>
     </form>
   );
